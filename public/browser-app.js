095241b1169cc7dc504499afb8dfe1bb556a959b
pritam0721 <pritamgayen629@gmail.com>
2024-07-22 19:39:31 +0530

    loading issue

--- public/browser-app.js
+++ public/browser-app.js
@@ -6,7 +6,7 @@ const total_amount = 10998;
 const shipping_fee = 1099;
 
 var stripe = Stripe(
-  'pk_test_51I87djFp5pnuKUXgBVIHiR36vVAWyfuyb7ckrhgyDNA1kM0GWHas9ZGUAgwJSFNUxrbyE6NwlMNmls1iGSfzHDdE00DQB3y6AH'
+  "pk_test_51NihgfSJnqVdvWLJ9KX5zpLBAWmmsRvk4vef6uP2OzVVyTO5DzpAdY2XABDFfRxIxoYsA66slZeAJaHs9q6gX6vt00K2vLzE4e"
 );
 
 // The items the customer wants to buy
